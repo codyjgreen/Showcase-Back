300eaf00cd298f8bb3aeab5920fe2bca47957914
AustinBH <harlowaustin2@gmail.com>
2019-11-01 16:09:57 -0700

    added models and rails auto generated standard tests, need to check into converting them to rspec tests

---
+++ Showcase-Back/db/migrate/20191101230218_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :cohort
+      t.string :linkedin
+      t.string :github
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
