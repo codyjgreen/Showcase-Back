98b03604c05e2f2dc79d7c60747a5e27fdfabd43
AustinBH <harlowaustin2@gmail.com>
2019-11-02 12:12:49 -0700

    changed deprecated factory girl gem to factory bot + added admin_user file per jason's suggestion

--- Showcase-Back/db/schema.rb
+++ Showcase-Back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_230524) do
+ActiveRecord::Schema.define(version: 2019_11_01_232701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2019_11_01_230524) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved"
+    t.string "about"
   end
 
 end
